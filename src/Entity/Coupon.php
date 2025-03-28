6b53d35926f94d30567b9ef446c419acd8f7dadf
vbudai <vbudai297@gmail.com>
2025-03-29 00:13:16 +0500

    Add entity, repository and migration for coupon table

---
+++ src/Entity/Coupon.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CouponRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CouponRepository::class)]
+#[ORM\UniqueConstraint(name: 'uniq_code', columns: ['code'])]
+#[ORM\Index(name: 'idx_code', columns: ['code'])]
+class Coupon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 64)]
+    private ?string $code = null;
+
+    #[ORM\Column]
+    private ?int $discount = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getDiscount(): ?int
+    {
+        return $this->discount;
+    }
+
+    public function setDiscount(int $discount): static
+    {
+        $this->discount = $discount;
+
+        return $this;
+    }
+}
