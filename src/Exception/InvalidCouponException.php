619ced2aaf217f2af3eb5cc54bdd3456cdc7247e
vbudai <vbudai297@gmail.com>
2025-03-31 11:38:18 +0500

    Removed unnecessary arguments from InvalidCouponException

--- src/Exception/InvalidCouponException.php
+++ src/Exception/InvalidCouponException.php
@@ -7,9 +7,7 @@ class InvalidCouponException extends \Exception
     public function __construct(string $couponCode)
     {
         parent::__construct(
-            sprintf('Invalid coupon code: %s', $couponCode),
-            $code,
-            $previous
+            sprintf('Invalid coupon code: %s', $couponCode)
         );
     }
 }
